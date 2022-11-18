605538a3cd433565b48e8a4ab484686f9df1a7ec
nagata-ichiko <eiichiro.kinoshita@i-standard.jp>
2022-11-18 16:56:04 +0900

    NuxtWebサイトサンプル

---
+++ nuxtportfolio/pages/blog/[id].vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="hero">
+            <img :src="data.image" alt="blog-image">
+        </div>
+        <div class="wrapper">
+            <div class="container">
+                <h1>
+                    {{ data.title }}
+                </h1>
+                <p>
+                    {{ data.date }}
+                </p>
+                <ContentDoc />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const { data } = await useAsyncData("/blog/firstblog", () => 
+    queryContent(useRoute().path).findOne())
+</script>
